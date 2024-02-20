b1a87edb409bc09bb539b24289e531b75bb4f465
xLennnix <moritzmeissgeier@gmail.com>
2024-02-20 10:21:46 +0100

    Reorganisation
    Projectile Class created Seperately

--- src/js/classes.js
+++ src/js/classes.js
@@ -85,36 +85,7 @@ class Enemy {
     }
 }
 
-class Projectile {
-    constructor({position = {x: 0,y:0}, enemy}){
-        this.position = position
-        this.velocity = {
-            x: 0,
-            y: 0
-        }
-        this.enemy = enemy
-        this.radius = 10
-        this.speed = 5
-    }
 
-    draw(){
-        c.beginPath()
-        c.arc(this.position.x, this.position.y, this.radius, 0, Math.PI * 2)
-        c.fillStyle = 'orange'
-        c.fill()
-    }
-
-    update(){
-        this.draw()
-
-        const angle = Math.atan2(this.enemy.center.y -this.position.y, this.enemy.center.x -this.position.x)
-        this.velocity.x = Math.cos(angle) * this.speed
-        this.velocity.y = Math.sign(angle) * this.speed
-
-        this.position.x += this.velocity.x
-        this.position.y += this.velocity.y
-    }
-}
 
 class Cat {
     constructor({position = {x:0,y:0}}){
